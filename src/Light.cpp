d871d21dc65ed020e470e66189d1ca713505d44b
Alex Hoover <ahoover@localhost.localdomain>
2014-11-17 20:53:07 -0500

    Took out namespace in implementation

--- src/Light.cpp
+++ src/Light.cpp
@@ -3,30 +3,26 @@
 
 using namespace gssmraytracer::lights;
 
-namespace gssmraytracer{
-namespace lights {
-  class Light::Impl {
-  public:
-    float intensity;
-    utils::Color color;
-  };
+class Light::Impl {
+public:
+  float intensity;
+  utils::Color color;
+};
 
-  Light::Light() : mImpl(new Impl) {
-    mImpl->intensity = 1;
-    mImpl->color = utils::Color();
-  }
-
-  Light::Light(utils::Color c, float intensity) : mImpl(new Impl) {
-    mImpl->intensity = intensity;
-    mImpl->color = c;
-  }
-
-  float Light::intensity() const {
-    return mImpl->intensity;
-  }
-
-  utils::Color Light::color() const {
-    return mImpl->color;
-  }
+Light::Light() : mImpl(new Impl) {
+  mImpl->intensity = 1;
+  mImpl->color = utils::Color();
 }
+
+Light::Light(utils::Color c, float intensity) : mImpl(new Impl) {
+  mImpl->intensity = intensity;
+  mImpl->color = c;
+}
+
+float Light::intensity() const {
+  return mImpl->intensity;
+}
+
+utils::Color Light::color() const {
+  return mImpl->color;
 }
